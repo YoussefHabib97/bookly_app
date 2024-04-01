959965a873130fa11c6315b2a28a8057d188e78b
Youssef <ykmhabib97@gmail.com>
2024-04-01 02:05:08 +0200

    Initial commit, created splash view's skeleton

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
