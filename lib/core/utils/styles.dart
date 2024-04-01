adcc3e82bbcf455d1a0d679aa7198742e345a4d8
Youssef <ykmhabib97@gmail.com>
2024-04-01 11:26:53 +0200

    Finished text styles for app

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:bookly_app/constants.dart';
 
 abstract class Styles {
+  static const TextStyle textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
   static const TextStyle textStyle18 = TextStyle(
     fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle textStyle20 = TextStyle(
+    fontSize: 20,
+    fontFamily: kGtSectraFine,
+    fontWeight: FontWeight.normal,
   );
 }
