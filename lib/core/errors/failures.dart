63291138e9f29940000fee0985d20935f45900d5
Youssef <ykmhabib97@gmail.com>
2024-04-03 01:03:25 +0200

    Finished home repository

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 
 abstract class Failure {
   final String errMessage;
-
   const Failure(this.errMessage);
 }
 
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
-import 'package:dio/dio.dart';
 
 class HomeRepositoryImplementation implements HomeRepository {
   final ApiService apiService;
@@ -14,7 +14,8 @@ class HomeRepositoryImplementation implements HomeRepository {
   Future<Either<Failure, List<BookModel>>> fetchLatestBooks() async {
     try {
       var data = await apiService.get(
-          endPoint: 'volumes?q=subject:programming&sorting=newest');
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming&sorting=newest');
 
       List<BookModel> booksList = [];
 
@@ -32,8 +33,24 @@ class HomeRepositoryImplementation implements HomeRepository {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+
+      List<BookModel> booksList = [];
+
+      for (var book in data['items']) {
+        booksList.add(BookModel.fromJson(book));
+      }
+
+      return right([...booksList]);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    } // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
 }
