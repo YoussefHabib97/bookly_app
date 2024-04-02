ee310970d368ca1e36c010b262b2b1f6a8985dbb
Youssef <ykmhabib97@gmail.com>
2024-04-03 01:48:28 +0200

    Created latest books cubit methods

--- lib/features/home/presentation/manager/latest_books_cubit/latest_books_cubit.dart
+++ lib/features/home/presentation/manager/latest_books_cubit/latest_books_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
@@ -5,5 +6,21 @@ import 'package:bookly_app/features/home/data/models/book_model/book_model.dart'
 part 'latest_books_state.dart';
 
 class LatestBooksCubit extends Cubit<LatestBooksState> {
-  LatestBooksCubit() : super(LatestBooksInitial());
+  LatestBooksCubit(this.homeRepository) : super(LatestBooksInitial());
+
+  final HomeRepository homeRepository;
+
+  Future<void> fetchLatestBooks() async {
+    emit(LatestBooksLoading());
+    var result = await homeRepository.fetchLatestBooks();
+
+    result.fold(
+      (failure) => emit(
+        LatestBooksFailure(failure.errMessage),
+      ),
+      (books) => emit(
+        LatestBooksSuccess(books),
+      ),
+    );
+  }
 }
