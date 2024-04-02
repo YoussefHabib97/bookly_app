a86698b7c80aaf24a31f0636a6d95d8651e54714
Youssef <ykmhabib97@gmail.com>
2024-04-03 01:49:34 +0200

    Finalized cubits

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
-import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 
 part 'featured_books_state.dart';
 
@@ -13,6 +13,7 @@ class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
   Future<void> fetchFeaturedBooks() async {
     emit(FeaturedBooksLoading());
     var result = await homeRepository.fetchFeaturedBooks();
+
     result.fold(
       (failure) => emit(
         FeaturedBooksFailure(failure.errMessage),
--- lib/features/home/presentation/manager/latest_books_cubit/latest_books_cubit.dart
+++ lib/features/home/presentation/manager/latest_books_cubit/latest_books_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 
 part 'latest_books_state.dart';
 
